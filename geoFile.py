feae3065455049ff97b1aa0d9eb53fd6e1329f6a
Juan C <jc3265@nova.edu>
2016-07-12 15:37:40 -0400

    Created the functions
    Created the functions to read and check the files

--- geoFile.py
+++ geoFile.py
@@ -4,9 +4,10 @@ import csv         #Csv library
 import requests    #Url Requests library
 import re          #Return Library
 Key = 'AIzaSyDYElSVqyQ9lUg9ksctNXg8JiSj_6-hEVU'
+myTextFile = '/home/zrtho/Documents/pythonTest/output1.txt'
 
 ####################################################################################
-#Create Function that looks for the nearby places of a given latitude and longitude#
+#   Function that looks for the nearby places of a given latitude and longitude    #
 ####################################################################################
 def build_URL(lat,longit):
     base_url = 'https://maps.googleapis.com/maps/api/place/radarsearch/json?location='     # Can change json to xml to change output type
@@ -20,11 +21,35 @@ def build_URL(lat,longit):
     url = base_url+latitude+','+longitude+radiusString+typeString+key_string
     return url
 
+#####################################################
+#   Function that Writes the result in json format  #
+#####################################################
+def writeToFile(url):
+    text_file = open(myTextFile, "w")#Text file where the output will be stored
+    #text_file.write(str(requests.get(urlResult).text) #Prints nicely in text format
+    text_file.write(str(requests.get(urlResult).json()))
+    text_file.close() #close file so that every line is a new file
+
+
+def checkTheFile():
+    fileToRead = open(myTextFile,"r")#Read the same file and scan for a string
+    for line in fileToRead:
+        if 'place_id' in line:
+            #print "found"
+            #print line.split('place_id')[1]
+            place_id = line.split('place_id')[1].replace("',","u'").split("u'")[1]
+            return place_id
+        else:
+            #print "Not Found"
+            return "Road"
+        #print tid
+    fileToRead.close()
+
 
 with open('/home/zrtho/Documents/csvFilesWithTimeStamp/place.csv', 'rb') as csvfile:
-    myFile = csv.reader(csvfile, delimiter=',')
-    for row in myFile:
-        text_file = open('/home/zrtho/Documents/pythonTest/output1.txt', "w")#Text file where the output will be stored
+    myCsvFile = csv.reader(csvfile, delimiter=',')
+    for row in myCsvFile:
+        #text_file = open('/home/zrtho/Documents/pythonTest/output1.txt', "w")#Text file where the output will be stored
         tid = int(row[0])
         latitude = row[1]
         longitude = row[2]
@@ -32,21 +57,12 @@ with open('/home/zrtho/Documents/csvFilesWithTimeStamp/place.csv', 'rb') as csvf
         time = row[4]
         gid = row[5]
         urlResult= build_URL(latitude,longitude)
-
-        #text_file.write(urlResult).text
-        text_file.write(str(requests.get(urlResult).json()))
-        text_file.close() #close file so that every line is a new file
-        fileToRead = open('/home/zrtho/Documents/pythonTest/output1.txt',"r")#Read the same file and scan for a string
+        writeToFile(urlResult)
+        place_id = checkTheFile()
+        print place_id
         #with open('fileToRead', 'r')
 
-        #for line in fileToRead:
-        #    if 'place_id' in line:
-    #            print line
-        #        print tid
-        #else:
-        #    print 'Not here' + line
-        #    print tid
-        #fileToRead.close()
+
 #        print req.startswith('place_id')
 '''
         someT = re.findall(r'place_id', oneLine)
